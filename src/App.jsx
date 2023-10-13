3d2a54d4841dbbe8cda1240c20d8b5ffa2662b21
Applebees1 <104962202+Applebees1@users.noreply.github.com>
2023-10-13 16:03:40 -0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const[meals, setMeals] = useState([]); 
+  
+  useEffect(() => {
+    //go get the data, put it in state
+    axios({
+      method: "GET",
+      url:"http://localhost:3000/meals",
+    }).then((response) => {
+      setMeals(response.data)
+      console.log(response.data)
+    });
+  }, [])
+
+  // const handleClick= () => {
+  //   //go get data from server
+  //   axios("http://localhost:3000/points").then((response) => {
+  //     //put data in state
+  //     (response.data.yourHealthScore)
+  //   })
+  // }
+
+  return (
+    <>
+     <h1>My Meals app() </h1>
+     {meals.map((meal) => { 
+      return(
+       <div key={JSON.stringify(meal)}>
+        <div>{meal.name}</div>
+        <div>{meal.instructions}</div>
+       </div>
+      )
+     })}
+      {/* <button onClick={handleClick}> Show Health Points </button>
+      {hp && <div>{hp}</div>} */}
+     {/* SHOW MEALS HERE */}
+    </>
+  )
+}
+
+export default App
